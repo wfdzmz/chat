abe8f67b8c18b057df6fd66deadd4470d17e13c1
wfd <374363679@qq.com>
2023-05-26 23:41:04 +0800

    聊天页面

---
+++ app/src/main/java/com/wfd/qq/message/Chat_Fragment.kt
@@ -0,0 +1,19 @@
+package com.wfd.qq.page
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.qq.R
+
+class Chat_Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_chat, container, false)
+        return view
+    }
+}
